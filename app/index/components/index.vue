e54bc2ca7ebf0c600e73d299d31f97a134975dc8
Kingsley <yuxiaochen@guanaihui.com>
2016-07-04 15:37:11 +0800

    add files

---
+++ app/index/components/index.vue
@@ -0,0 +1,26 @@
+<style>
+body {
+  font: Helvetica, sans-serif;
+  color: #999;
+}
+.logo {
+  width: 40px;
+  height: 40px;
+}
+</style>
+
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello from another module!'
+    }
+  }
+}
+</script>
